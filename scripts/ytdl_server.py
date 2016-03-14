4130d2c75867866f4f1b2bb4077c66a25ff0524a
Dan Elkouby <streetwalrus@codewalr.us>
2016-03-14 11:51:32 +0200

    ytdl_server: add some exception handling

--- scripts/ytdl_server.py
+++ scripts/ytdl_server.py
@@ -9,18 +9,21 @@ import ytdl_config as config
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        parsed_params = parse.urlparse(self.path)
-        parsed_query = parse.parse_qs(parsed_params.query)
-        video_url = parsed_query['i'][0]
+        try:
+            parsed_params = parse.urlparse(self.path)
+            parsed_query = parse.parse_qs(parsed_params.query)
+            video_url = parsed_query['i'][0]
 
-        command = list(map(str, config.PLAYER.split(' ')))
-        command.append(video_url)
+            command = list(map(str, config.PLAYER.split(' ')))
+            command.append(video_url)
 
-        player = subprocess.Popen(command, stdout=DEVNULL, stderr=DEVNULL)
+            player = subprocess.Popen(command, stdout=DEVNULL, stderr=DEVNULL)
 
-        self.send_response(204)
-
-        player.wait()
+            self.send_response(204)
+        except:
+            pass
+        finally:
+            player.wait()
 
     def log_message(self, format, *args):
         # Disable debug output
