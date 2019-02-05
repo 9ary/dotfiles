8d3be67374236b5f3cfadd7845345b33668abff7
Dan Elkouby <streetwalkermc@gmail.com>
2019-02-05 23:46:27 +0200

    i3/fullscreen-manager: update special case for Telegram

--- i3/fullscreen-manager.py
+++ i3/fullscreen-manager.py
@@ -10,6 +10,7 @@ notify_unpause = Notify.Notification.new("DUNST_COMMAND_RESUME", "", "dialog-inf
 def on_window(self, e):
     if e.container.window_class == "TelegramDesktop" \
             and e.container.name == "Media viewer":
+        e.container.command("fullscreen disable")
         e.container.command("move absolute position center")
         return
 
