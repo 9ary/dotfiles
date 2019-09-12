7cee2c961e0ee27018b1d310d0b8f52385d328b6
Dan Elkouby <streetwalkermc@gmail.com>
2019-09-12 17:34:07 +0300

    scripts/nowplaying.py: refactor and unify with weechat plugin
    Now that weechat uses python 3 for scripting we can unify these two
    largely identical scripts.

--- scripts/nowplaying.py
+++ scripts/nowplaying.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+try:
+    import weechat
+except ModuleNotFoundError:
+    weechat = None
 import json
 import socket
 import subprocess
@@ -11,72 +15,77 @@ def mpv_query():
             client.connect(os.path.join(os.getenv("HOME"), ".mpv/socket"))
 
             res = {}
-            for i in ['media-title', 'pause']:
-                req = '{"command": ["get_property", "%s"]}\n' %i
-                client.send(req.encode("utf-8"))
-                try:
-                    res[i] = json.loads(client.recv(1024).decode("utf-8"))['data']
-                except KeyError:
-                    res[i] = None
+            for prop in ("media-title", "pause"):
+                req = {"command": ["get_property", prop]}
+                client.send(f"{json.dumps(req)}\n".encode())
+                res[prop] = json.loads(client.recv(1024).decode()).get("data")
 
-        return res['pause'], res['media-title']
+        return not res["pause"], res["media-title"]
     except socket.error:
-        return True, None
+        return False, None
 
 def mpd_command(client, cmd):
-    client.send((cmd + "\n").encode("utf-8"))
+    client.send(f"{cmd}\n".encode())
     response = client.recv(1024).decode().split("\n")
 
-    res = {}
-    for line in response:
-        line = line.split(": ")
-        res[line[0]] = ": ".join(line[1:])
+    def pairs(lines):
+        for line in lines:
+            if line == "OK":
+                return
+            yield line.split(": ", 1)
 
-    return res
+    return {k: v for k, v in pairs(response)}
 
 def mpd_query():
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
         client.connect(os.path.join(os.getenv("HOME"), ".mpd/socket"))
-
-        # Discard the initial message
-        client.recv(1024)
-
-        status = mpd_command(client, "status")
+        client.recv(1024) # Discard the initial message
 
         song = mpd_command(client, "currentsong")
+        title = song.get("Title")
+        artist = song.get("Artist")
+        status = mpd_command(client, "status")
 
-        try:
-            title = song["Title"]
-        except:
-            title = "Null"
+        client.send("close\n".encode())
 
-        try:
-            artist = song["Artist"]
-        except:
-            artist = "Null"
+        return status["state"] == "play", f"{artist} - {title}"
 
-        client.send("close\n".encode("utf-8"))
+def get_title():
+    for playing, title in (mpd_query(), mpv_query()):
+        if playing and title is not None:
+            break
+    if title is not None:
+        return f"/me np: {title}"
 
-        if status["state"] != "play":
-            return True, None
-        else:
-            return False, f"{artist} - {title}"
+def copy_title():
+    title = get_title()
+    if title is not None:
+        subprocess.run(["xsel", "-b"], input=title.encode(), check=True)
 
-def copy_title(title):
-    title = f"/me np: {title}".encode()
-    #subprocess.run(["xsel", "-x"], check=True)
-    subprocess.run(["xsel", "-b"], input=title, check=True)
-    subprocess.run(["xdotool", "keyup", "--clearmodifiers", "Return"], check=True)
-    subprocess.run(["xdotool", "key", "--clearmodifiers", "ctrl+v"], check=True)
-    #subprocess.run(["xsel", "-x"], check=True)
+def weechat_np(data, buffer, args):
+    title = get_title()
+    if title is not None:
+        weechat.command(buffer, title)
+    return weechat.WEECHAT_RC_OK
 
-def main():
-    mpd_pause, mpd_title = mpd_query()
-    mpv_pause, mpv_title = mpv_query()
-    if not mpd_pause and mpd_title is not None:
-        copy_title(mpd_title)
-        return
-    if mpv_title is not None:
-        copy_title(mpv_title)
-
-main()
+if weechat is not None:
+    weechat.register(
+        "nowplaying",
+        "Streetwalrus",
+        "0.1",
+        "The game",
+        "Now playing",
+        "",
+        ""
+    )
+    weechat.hook_command(
+        "np",
+        "Now playing",
+        "",
+        "",
+        "",
+        "weechat_np",
+        ""
+    )
+elif __name__ == "__main__":
+    copy_title()
