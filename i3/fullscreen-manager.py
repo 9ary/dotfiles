25a87124dd5787a28683fc6b9f9e2e04679647bb
Dan Elkouby <streetwalkermc@gmail.com>
2019-10-08 10:47:34 +0300

    Update dunstrc

--- i3/fullscreen-manager.py
+++ i3/fullscreen-manager.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 
 import i3ipc
-from gi.repository import Notify
-
-Notify.init("i3-fullscreen-manager")
-notify_pause = Notify.Notification.new("DUNST_COMMAND_PAUSE", "", "dialog-information")
-notify_unpause = Notify.Notification.new("DUNST_COMMAND_RESUME", "", "dialog-information")
 
 def on_window(self, e):
     if e.container.window_class == "TelegramDesktop" \
@@ -13,12 +8,6 @@ def on_window(self, e):
         e.container.command("fullscreen disable")
         e.container.command("move absolute position center")
         return
-
-    if e.container.focused:
-        if e.container.fullscreen_mode:
-            notify_pause.show()
-        else:
-            notify_unpause.show()
 
     screenrect = conn.get_outputs()[0].rect
     w, h = screenrect.width, screenrect.height
