a361e4ac1cbeeaf01021429be0dd1b3d4a228f0a
Dan Elkouby <streetwalkermc@gmail.com>
2019-12-27 22:29:49 +0200

    Fully embrace sway

---
+++ i3/startup.zsh
@@ -0,0 +1,35 @@
+#!/usr/bin/zsh
+
+setopt nobgnice
+
+systemd_setup=/etc/sway/conf.d/10-systemd.conf
+[[ -x $systemd_setup ]] && $systemd_setup
+
+xrdb ~/dotfiles/x/Xresources
+
+import-gsettings \
+    gtk-theme:gtk-theme-name \
+    icon-theme:gtk-icon-theme-name \
+    cursor-theme:gtk-cursor-theme-name \
+    font-name:gtk-font-name
+
+udiskie -s &
+redshift &
+pidof ydotoold || ydotoold &
+~/dotfiles/i3/fullscreen-manager.py &
+
+$BROWSER &
+$TERMINAL -name WeeChat -e weechat &
+telegram-desktop &
+qbittorrent &
+
+if [[ "$HOST" == "Akatsuki" ]]; then
+    dunst -config ~/dotfiles/dunstrc &
+fi
+
+if [[ "$HOST" == "Hitagi" ]]; then
+    dunst -config ~/dotfiles/dunstrc \
+        -geometry "300x5-20+20" -font "Ubuntu Mono 11.5" &
+    libinput-gestures &
+    swayidle -w before-sleep 'swaylock -fFc 333333' &
+fi
