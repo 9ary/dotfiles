db955c5b8dbb0e4cf5ec39292c3cdd3f659cf34f
novenary <streetwalkermc@gmail.com>
2021-08-26 13:57:32 +0300

    autoloopback: sleep 5s at startup to let pulseaudio initialize

--- autoloopback/src/main.rs
+++ autoloopback/src/main.rs
@@ -14,6 +14,9 @@ const RMS_THRESHOLD_EXPANDED: f64 = RMS_THRESHOLD * RMS_THRESHOLD * BUFFER_FRAME
 const HYSTERESIS: usize = (SAMPLE_RATE / BUFFER_FRAMES * 3) as usize;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // Give Pulse a chance to open the card before us
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
     let mixer = alsa::Mixer::new(CARD_NAME, false)?;
     let sid = alsa::mixer::SelemId::new("Loopback Mixing", 0);
     let selem = mixer.find_selem(&sid).ok_or("Mixer element not found")?;
@@ -36,7 +39,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     loop {
         let mut buf: [i16; (BUFFER_FRAMES * CHANNELS) as usize] = unsafe {
-            std::mem::uninitialized()
+            std::mem::MaybeUninit::uninit().assume_init()
         };
         let frames = io.readi(&mut buf)?;
         let acc = buf[..frames * CHANNELS as usize].iter().map(|&s| s as f64 * s as f64).sum();
